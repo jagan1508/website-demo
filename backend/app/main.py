c814a66a35b903c0d677f6b7a1d4065c1c964ed6
jagan1508 <jaganathbabu025@gmail.com>
2026-05-29 16:47:10 +0530

    containerised backend and db

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,8 @@ from app.core.database import Base, engine
 from app.models.product import Product
 from app.models.category import Category
 
+from fastapi.middleware.cors import CORSMiddleware
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Art Store API")
@@ -15,3 +17,15 @@ app.include_router(api_router)
 @app.get("/")
 def root():
     return {"message": "Art Store Backend Running"}
+
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
